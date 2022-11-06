c42974f010b99287860e9afa72757f7fa87338b2
naveenrajm7 <naveenrajm97@gmail.com>
2022-11-06 16:51:45 -0700

    skip libbpf clone

--- xdp-setup/scripts/install-libbpf.sh
+++ xdp-setup/scripts/install-libbpf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Get XDP tutorial with libbpf (sub module)
-git clone --recurse-submodules https://github.com/xdp-project/xdp-tutorial
-
 # Update packges
 sudo apt-get update
 
@@ -18,3 +15,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y linux-headers-$(uname -r)
 # Recommended tools, bpftool , tcpdump
 sudo DEBIAN_FRONTEND=noninteractive apt install -y linux-tools-common linux-tools-generic
 sudo apt install -y tcpdump
+
+# Get XDP tutorial with libbpf (sub module)
+# git clone --recurse-submodules https://github.com/xdp-project/xdp-tutorial
